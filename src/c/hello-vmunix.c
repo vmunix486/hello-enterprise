aaf4692a26314a0befec332232dc44c10ee5656a
Marco Esquandolas <justaguy8086@gmail.com>
2026-06-15 15:20:16 -0400

    Add bloated hello world that I made

---
+++ src/c/hello-vmunix.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+int h()
+{
+	printf("H");
+}
+
+int e()
+{
+	printf("e");
+}
+
+int l()
+{
+	printf("l");
+}
+
+int o()
+{
+	printf("o");
+}
+
+int w()
+{
+	printf("W");
+}
+
+int r()
+{
+	printf("r");
+}
+
+int d()
+{
+	printf("d");
+}
+
+int comma()
+{
+	printf(",");
+}
+
+int space()
+{
+	printf(" ");
+}
+
+int exclamation()
+{
+	printf("!");
+}
+
+int newline()
+{
+	printf("\n");
+}
+
+int funcreturn()
+{
+	return 0;
+}
+
+int main(void)
+{
+	h();
+
+	e();
+	
+	l();
+	l();
+
+	o();
+
+	comma();
+
+	space();
+
+	w();
+
+	o();
+
+	r();
+
+	l();
+
+	d();
+
+	exclamation();
+
+	newline();
+
+	return 0;
+}
